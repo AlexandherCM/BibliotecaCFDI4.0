e97fcfd17a2ec5f52a51f4c94a850fa35239b087
AlexandherCM <alexandhercordoba378@gmail.com>
2025-04-01 18:38:39 -0600

    [CANCELACIÓN DE FACTURAS PDT]

--- CFDI4.0/CFDI4.0/obj/Debug/netstandard2.0/CFDI4.0.AssemblyInfo.cs
+++ CFDI4.0/CFDI4.0/obj/Debug/netstandard2.0/CFDI4.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CFDI4.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38332fd1887ad5fb517858e620cfdb77b6ad0e4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf95bd4d5a8bbcfb7acebf7d6b54784a74a31ea6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CFDI4.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CFDI4.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
